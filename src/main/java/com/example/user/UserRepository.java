2b973a9e18fe8bb99a65d47d05bf75fcd1f0c74e
alinalinux <soltanicialina777@gmail.com>
2024-09-24 13:18:07 +0300

    added some features

---
+++ src/main/java/com/example/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.user;
+
+import com.example.user.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> { // <User, Long> user is the entity with which i work, but Long is data type of Id in the entity.
+
+}
