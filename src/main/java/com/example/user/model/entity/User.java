b3451c9b71cd2ca9a0472b9826e07d9baa1c7bba
alinalinux <soltanicialina777@gmail.com>
2024-09-24 10:45:57 +0300

    initial commit

---
+++ src/main/java/com/example/user/model/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.user.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name")
+    private String username;
+    @Column(name = "password")
+    private String password;
+}
